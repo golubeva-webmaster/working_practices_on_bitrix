6f08a037608dc3ac8eedc5e5ce1b6ba5fdc0d9e0
Marina <60580475+golubeva-webmaster@users.noreply.github.com>
2021-02-18 11:09:44 +0300

    Create index.js

---
+++ js/orderInsertUserInfo/index.js
@@ -0,0 +1,33 @@
+/** Возвращает true, если пользователь находится на странице оформления заказа */
+function isOrderPage() {
+	return (document.location.pathname =='/order/') ? true : false;
+}
+  
+if(isOrderPage()){
+		// Подстановка данных физ и юр лиц на странице оформления заказа
+
+		$.ajax({
+			type: 'POST',
+			url: '/local/templates/main/ajax/user-get-info.php',
+			data: '',
+			dataType: 'json',
+		}).done(function(res){
+			
+			if ("status" in res) {
+				if(res.status === 'success') {
+					//console.log("Пользователь авторизован, данные взялись из системы %o", res);
+					$('input[name="ORDER_PROP_64"]').val(res.fio); // FIO fiz
+					$('input[name="ORDER_PROP_67"]').val(res.address); // Address fiz
+					$('input[name="ORDER_PROP_76"]').val(res.address); // Address ur
+					$('input[name="ORDER_PROP_71"]').val(res.companyName); // companyName ur
+					$('input[name="ORDER_PROP_73"]').val(res.contactFace); // contactFace ur
+					$('input[name="ORDER_PROP_36"]').val(res.ogrn);
+					$('input[name="ORDER_PROP_39"]').val(res.okpo);
+					$('input[name="ORDER_PROP_34"]').val(res.inn);
+					$('input[name="ORDER_PROP_35"]').val(res.kpp);
+					$('input[name="ORDER_PROP_40"]').val(res.companyRuk);
+				}
+				else console.log("Пользователь не авторизован %o", res);
+			}
+		});
+	}
