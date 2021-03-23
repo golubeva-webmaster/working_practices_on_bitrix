94fa57d8d901275e77e6d1b2428463568415bf35
Marina <60580475+golubeva-webmaster@users.noreply.github.com>
2021-03-23 08:54:00 +0300

    Create custom.js

---
+++ js/sber/custom.js
@@ -0,0 +1,19 @@
+/*.....*/
+	// для сообщений о Сбер авторизации
+	$("#error-sber-auth-link #success-sber-auth-link").fancybox({
+		width		: 500,
+		height		: 300,
+		autoSize	: true,
+		closeClick	: false,
+		topRatio: 0.5,
+	});
+
+	// для окна со Сбер кредитными предложениями
+	$("#sber-credit-offers-link").fancybox({
+		width		: document.documentElement.clientWidth*0.8,
+		height		: document.documentElement.clientHeight*0.8,
+		autoSize	: true,
+		closeClick	: false,
+		topRatio: 0.5,
+	});
+/*.....*/
